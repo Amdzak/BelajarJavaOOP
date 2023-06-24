8a56545d933006fb7ed96ca642361181418755bd
Amdzak <58507217+Amdzak@users.noreply.github.com>
2023-06-24 22:11:41 +0700

    Create Main.java

---
+++ 09. MultiFile dengan Package/Main.java
@@ -0,0 +1,25 @@
+package project;
+
+/* 
+ * Mulfi file dengan package 
+ * 
+ * dalam materi nin kita akan membuat file lain dengan package atau lokasi yang berbeda
+ * yang mana seperti kita membuat file CSS baru di tempat lain lalu kita akses 
+ * jika penampatan file tersebut dalam 1 folder yang saam maka pengaksesan sebuah file dapat di akses dengan memanggil classnya saja
+ * 
+ * jika file tersebut ada pada sebuah folder terpisah maka kita dapat mengaksesnya dengan menggunkaan import
+ * tinggal kita tulisa saja seperti ini 
+ *      import namaFolder.namaFolder.NamaFile;
+ */
+
+import com.Terminal.Console;
+
+public class main{
+    public static void main(String args[]){
+        Player pemain1 = new Player("Minato");
+
+        pemain1.show();
+
+        Console.log("Hello World");
+    }
+}
