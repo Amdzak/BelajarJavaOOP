55e130aaee11a1bd632b5902b0eecb95dc83ba52
Amdzak <58507217+Amdzak@users.noreply.github.com>
2023-06-25 12:39:39 +0700

    Create Console.java

---
+++ 10. Hierarkie Package/terminal/Console.java
@@ -0,0 +1,9 @@
+package com.terminal;
+
+
+// dia mengunakan public supaya bisa di akses oleh class main yang berada pada folder yang beda
+public class Console {
+    public static void log(String nama){
+        System.out.println(nama);
+    }
+}
