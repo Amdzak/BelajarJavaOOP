696edd1c99e5a025457ef2b8c8ba4e0cebbf860e
Amdzak <58507217+Amdzak@users.noreply.github.com>
2023-06-27 19:11:35 +0700

    Create Hero.java

---
+++ 14. Super (Inheritance)/Hero.java
@@ -0,0 +1,13 @@
+package com.project;
+
+public class Hero {
+    String name = "Class Hero";
+
+    void display(){
+        System.out.println("ini adalah " + this.name);
+    }
+
+    void dumyMethod(){
+        System.out.println("Method ini berada di superClass");
+    }
+}
