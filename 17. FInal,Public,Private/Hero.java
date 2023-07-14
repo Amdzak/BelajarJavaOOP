b588892526c314e29ccf8b5ab33966b84aaac9aa
Amdzak <58507217+Amdzak@users.noreply.github.com>
2023-07-14 09:25:52 +0700

    Create Hero.java

---
+++ 17. FInal,Public,Private/Hero.java
@@ -0,0 +1,34 @@
+package com.project;
+
+public class Hero {
+    public String nama;
+    private double nyawa;
+
+
+    Hero(String nama,double nyawa){
+        this.nama = nama;
+        this.nyawa = nyawa;
+    }
+
+    // getter
+    double getNyawa(){
+        return this.nyawa;
+    }
+
+    //setter dengan final
+    final double SetNyawa(double nyawa){
+        return this.nyawa = nyawa;
+    }
+
+    // overload setter 
+    void SetNyawa(String nama){
+        if (nama.equals("reset")) {
+            System.out.println("Nyawa telah di Reset");
+            this.nyawa = 100;
+        }
+    }
+
+    public void display(){
+        System.out.println(this.nama + " Memiliki Hp " + this.getNyawa()); //disini private nyawa dapat di ambil hanya dengan getter
+    }
+}
